50ed36f30fc042ebb9600509e270e7572708cef0
Samodya <samodya0203samadini@gmail.com>
2025-03-11 20:28:50 +0530

    Add files via upload

---
+++ Mathematical.js
@@ -0,0 +1,16 @@
+console.log("Mathematical Operations")
+
+//Besic Mathematical Operatoins
+
+let num1 = 10;
+let num2 = 5;
+
+let sum = num1 + num2;
+let sub = num1 - num2;
+let div = num1 / num2;
+let mul = num1 * num2;
+
+console.log("Summation is:" + sum);
+console.log("Subtraction is:" + sub);
+console.log("Division is:" + div);
+console.log("Multiplication is:" + mul);
